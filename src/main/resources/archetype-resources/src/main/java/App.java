915c4ecbf279a6781fbcde061572e0dd1549390c
BarkaBoss <bossmajor.biii@gmail.com>
2024-03-11 23:33:45 +0100

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ng.com.nokt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
